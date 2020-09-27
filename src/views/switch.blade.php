4a209f59464b65f40597cc16db0fc2d8e7d29d2e
ndsatu <mdsatu.pro@gmail.com>
2020-09-27 12:34:58 +0600

    fix check bug

--- src/views/switch.blade.php
+++ src/views/switch.blade.php
@@ -1,6 +1,7 @@
 @php
 $column = $column ?? config('switcher.default_column');
 $id = $id ?? $data->id;
+$status = $data[$column];
 @endphp
 
-<label class="switcher_switch"><input data-table="{{$table}}" data-column="{{$column}}" data-id="{{$id}}" type="checkbox" checked><span class="switcher_slider switcher_round"></span></label>
+<label class="switcher_switch"><input data-table="{{$table}}" data-column="{{$column}}" data-id="{{$id}}" type="checkbox" {{$status ? 'checked' : ''}}><span class="switcher_slider switcher_round"></span></label>
